085779695ea27c6745288ab841042cccbe56835c
costagavras <cgavras@yahoo.ca>
2018-09-28 13:58:59 -0400

    done

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -15,18 +15,31 @@ class EntriesController < ApplicationController
   end
 
   def create
+    @new_entry = Entry.new
+    @new_entry.word = params[:entry][:word]
+    @new_entry.definition = params[:entry][:definition]
+    @new_entry.language = params[:entry][:language]
+    @new_entry.save
     redirect_to entries_url
   end
 
   def edit
+    @entry = Entry.find(params[:id])
     render :edit
   end
 
   def update
+    @entry = Entry.find(params[:id])
+    @entry.word = params[:entry][:word]
+    @entry.definition = params[:entry][:definition]
+    @entry.language = params[:entry][:language]
+    @entry.save
     redirect_to entry_url(params[:id])
-  end
+   end
 
   def destroy
-    redirect_to :index
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+    redirect_to entries_path
   end
 end
